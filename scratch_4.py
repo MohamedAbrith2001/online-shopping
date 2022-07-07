eec201ca16c61fcb14213ce7fabd70c2cb83623c
MohamedAbrith2001 <108867197+MohamedAbrith2001@users.noreply.github.com>
2022-07-07 03:02:57 -0700

    Add files via upload

---
+++ scratch_4.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number:"))
+if n%2==0:
+    if n%3==0:
+        print("divisible by both 2 and 3")
+    else:
+        print("divisible by 2 not 3")
+else:
+    if n%3==0:
+        print("divisible by 3")
+    else:
+        print("not div by 2 and 3")
---
+++ scratch_5.py
@@ -0,0 +1,6 @@
+name=str(input("enter the name"))
+n=len(name)
+if n>25:
+    print("out of limit")
+else:
+    print(name)
